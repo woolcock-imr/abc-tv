7f89713213cc8b4352b0a47f954defbf1c1abbe2
Zhiming Zhou <zzm.vmiis@gmail.com>
2017-07-11 16:24:38 +1000

    update

--- modules/task/task.form.js
+++ modules/task/task.form.js
@@ -1,7 +1,12 @@
+//-------------------------------------
+var _ids=_config.module_ids;
+var sql_participant=_config.parameters.sql_participant;
+var participant_tid	=$vm.module_list[_ids.participant].table_id;
+//-------------------------------------
 $('#Participant__ID').autocomplete({
     minLength:0,
     source:function(request,response){
-        var sql="with tb as (select name=@('Subject_Initials')+' '+@('Gender')+' '+@('DOB'),value2=UID,value3=S1 from [FORM-"+$vm.module_list[_app_id+'participant']['table_id']+"])";
+        var sql="with tb as (select name="+sql_participant+",value2=UID,value3=S1 from [FORM-"+participant_tid+"])";
         sql+=" select top 10 name,value=name,value2,value3 from tb where Name like '%'+@S1+'%' ";
         $VmAPI.request({data:{cmd:'auto',s1:request.term,sql:sql,minLength:0},callback:function(res){
             response($vm.autocomplete_list(res.table));
